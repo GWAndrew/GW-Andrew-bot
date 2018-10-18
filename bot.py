8e493f275bed602cca152b97fd604142f2dacb90
GWAndrew <43089870+GWAndrew@users.noreply.github.com>
2018-10-18 19:42:27 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,9 +2,6 @@ import discord
 from discord.ext import commands
 from discord.ext.commands import Bot
 import asyncio
-import chalk
-import random
-import time
 import os
 
 Client = discord.Client()
