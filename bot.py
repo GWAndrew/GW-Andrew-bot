ec1438b0ad44fec5ac3495a0a9ba8f353cf04de5
GWAndrew <43089870+GWAndrew@users.noreply.github.com>
2018-12-07 18:49:27 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,7 +16,7 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-	if "Trillion List Count LMAO xD 123456789" in message.content.upper():
+	if "Trillion List Count LMAO xD 123456789" in message.content():
 		a = 0
 		while a != 123456789:
 			await client.send_message(message.channel, a)
