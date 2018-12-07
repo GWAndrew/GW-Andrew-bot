5fa76d9675ffce974f0130b228ef0d0989b2994d
GWAndrew <43089870+GWAndrew@users.noreply.github.com>
2018-12-07 18:27:44 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,19 +2,32 @@ import discord
 from discord.ext import commands
 from discord.ext.commands import Bot
 import asyncio
+import time
 import os
 
 Client = discord.Client()
-client = commands.Bot(command_prefix = "a!")
-@client.event
-async def on_ready():
-	print ("Connected")
-	await client.change_presence(game = discord.Game(name="Prefix : a!"))
+Client = commands.bot(command_prefix = "lmao!")
+
 
 @client.event
-async def on_message(message):
-	if message.content.upper() == ('OOF'):
-		msg = 'ARE YOU A DOG'
-		await client.send_message(message.channel, msg)
-				
-client.run(os.getenv(''))
+async def on_ready():
+    await client.change_presence(game=discord.Game(name='Prefix is lmao!'))
+        print('launched')
+
+@client.command 
+async def count(ctx):
+    print('Counting to 1 trillion idk why lol')
+    if ctx.message.author.server_permissions.administrator:
+        a = 0
+        for i in range(0, 1000000000000):
+        a = a + 1
+        print(a)
+        await client.say(a)
+        else:
+        a = 0
+        for i in range(0, 1000000000000):
+        a = a + 1
+        print(a)
+        await client.say(a)
+
+client.run(os.getenv("TOKEN"))
