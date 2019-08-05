ed909837b3199f3dcbb952d39f6bb16f893fa21c
GWAndrew <43089870+GWAndrew@users.noreply.github.com>
2019-08-05 18:48:45 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -38,6 +38,47 @@ async def ping(ctx):
     diff = resp.timestamp - ctx.message.timestamp
     await bot.say(f"{1000*diff.total_seconds():.1f}ms")
     print ("Ping")
+
+	
+	
+@bot.command(pass_context=True)
+async def gay(ctx, arg1):
+	#sadly the bot detector doesnt work yet I have to fix this code lol
+	user=arg1
+	if user is not bot.user:
+		random_number=random.randint(0,100)
+		random_color=random.choice(color_list)
+		embed=discord.Embed(title=":gay_pride_flag: GAY RATE :gay_pride_flag:",description="{} IS {}% GAY".format(arg1, random_number), color=random_color)
+		msg = await ctx.bot.say(embed=embed)
+		print("{}% GAY".format(random_number))
+		loop = 0
+		while loop < 50:
+			random_color=random.choice(color_list)
+			embed=discord.Embed(title=":gay_pride_flag: GAY RATE :gay_pride_flag:",description="{} IS {}% GAY".format(arg1, random_number), color=random_color)
+			await ctx.bot.edit_message(msg,embed=embed)
+			loop=loop+1
+			await asyncio.sleep(1)
+	else:
+		embed=discord.Embed(title=":gay_pride_flag: GAY RATE :gay_pride_flag:",description="ARE BOTS GAY TOO ? :thinking:", color=0x62cee5)
+
+
+
+@gay.error
+async def gay_on_error(error,ctx):
+	random_number=random.randint(0,100)
+	random_color=random.choice(color_list)
+	embed=discord.Embed(title=":gay_pride_flag: GAY RATE :gay_pride_flag:",description="{}, YOU ARE {}% GAY".format(ctx.message.author,random_number), color=random_color)
+	msg = await bot.say(embed=embed)
+	print("{}% GAY".format(random_number))
+	loop = 0
+	while loop < 10:
+		random_color=random.choice(color_list)
+		embed=discord.Embed(title=":gay_pride_flag: GAY RATE :gay_pride_flag:",description="{}, YOU ARE {}% GAY".format(ctx.message.author,random_number), color=random_color)
+		await bot.edit_message(msg,embed=embed)
+		loop=loop+1
+		await asyncio.sleep(2)
+
+
        
 bot.run(os.getenv("TOKEN"))
 
